a347a8e9c71f742c040f691f5ce1893d33e7a069
Josh Goodman <jogoodma@iu.edu>
2020-04-10 00:51:28 -0400

    Implemented a basic card layout for vm list.

--- renderer/components/VmTableView/index.js
+++ renderer/components/VmTableView/index.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const VmTableView = ({ vmData = null, onRefresh = () => {} }) => {
-  const cms = vmData?.cms ?? null
   const output = vmData?.output ?? []
   return (
     <>
       <h2>VM Table View</h2>
       <button onClick={onRefresh}>Refresh</button>
       <table>
-        {output.map(({name, ip_public, status}) => (
-          <tr>
-            <td>{name}</td>
-            <td>{ip_public}</td>
-            <td>{status}</td>
-          </tr>
-        ))}
+        <tbody>
+          {output.map(({ id, name, ip_public, status }) => (
+            <tr key={id}>
+              <td>{name}</td>
+              <td>{ip_public}</td>
+              <td>{status}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   )
--- renderer/pages/vmlist.js
+++ renderer/pages/vmlist.js
@@ -3,7 +3,11 @@ import VmCardView from '../components/VmCardView'
 import VmTableView from '../components/VmTableView'
 import { ipcRenderer } from 'electron'
 import { CMS_VM_LIST_SEND } from '../../main/constants'
+// import { makeCancelable } from '../../main/utils'
 
+// TODO - Need to handle situations where the vm list command has fired
+//        and the user clicks away, unmounting the component.
+//        This results in setVmListResp getting called on an unmounted component
 const VmList = () => {
   const [mode, setMode] = useState('card')
   const [vmListResp, setVmListResp] = useState(null)
@@ -17,12 +21,27 @@ const VmList = () => {
   }
 
   useEffect(() => {
+    // const cancelablePromise = makeCancelable(
+    //   new Promise(r => component.setState({...}))
+    // );
+    //
+    // cancelablePromise
+    //   .promise
+    //   .then(() => console.log('resolved'))
+    //   .catch((reason) => console.log('isCanceled', reason.isCanceled));
+    //
+    // cancelablePromise.cancel(); // Cancel the promise
+    // const cancelationToken = {}
     const initialFetch = async () => {
       if (ipcRenderer) {
         setVmListResp(await ipcRenderer.invoke(CMS_VM_LIST_SEND))
       }
     }
     initialFetch()
+    // Called on unmount
+    return () => {
+      // Cancel all calls to setVmListResp here.
+    }
   }, [])
 
   const viewProps = {
