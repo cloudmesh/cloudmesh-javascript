1805b7dd2a6a5cc9f0af09e548c85970acaaa04e
Josh Goodman <jogoodma@iu.edu>
2020-04-14 01:07:18 -0400

    Added non-functional cloud provider button.

--- renderer/components/Sidebar/index.js
+++ renderer/components/Sidebar/index.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
+import FormControl from '@material-ui/core/FormControl'
+import InputLabel from '@material-ui/core/InputLabel'
+import Select from '@material-ui/core/Select'
 import Paper from '@material-ui/core/Paper'
 import MenuItem from '@material-ui/core/MenuItem'
 import MenuList from '@material-ui/core/MenuList'
 
 const Sidebar = () => {
+  const [cloudProvider, setCloudProvider] = useState('openstack')
+
+  const handleCloudProviderChange = (event) => {
+    setCloudProvider(event.target.value)
+  }
+
   return (
     <Paper>
       <MenuList>
@@ -20,9 +29,19 @@ const Sidebar = () => {
           </Link>
         </MenuItem>
         <MenuItem>
-          <Link href="/settings">
-            <a>Settings</a>
-          </Link>
+          <FormControl>
+            <InputLabel id="select-cloud-label">Cloud</InputLabel>
+            <Select
+              labelId="select-cloud-label"
+              id="cloud-select"
+              value={cloudProvider}
+              onChange={handleCloudProviderChange}>
+              <MenuItem value="openstack">OpenStack</MenuItem>
+              <MenuItem value="aws">AWS</MenuItem>
+              <MenuItem value="google">Google</MenuItem>
+              <MenuItem value="azure">Azure</MenuItem>
+            </Select>
+          </FormControl>
         </MenuItem>
       </MenuList>
     </Paper>
