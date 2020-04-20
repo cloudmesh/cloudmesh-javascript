dc6ba63a056c32fa595c2edaa275e95a93cf9c75
Gregor von Laszewski <laszewski@gmail.com>
2020-04-19 21:40:52 -0400

    forgot to check in the stop command

--- cloudmesh/viewer/command/viewer.py
+++ cloudmesh/viewer/command/viewer.py
@@ -21,6 +21,7 @@ class ViewerCommand(PluginCommand):
         ::
 
           Usage:
+                viewer install
                 viewer stop
                 viewer start [OPTIONS...]
 
@@ -29,8 +30,7 @@ class ViewerCommand(PluginCommand):
 
         """
 
-
-        if arguments.stop:
+        def stop():
             found = []
             processes = Shell.ps()
             for p in processes:
@@ -41,9 +41,17 @@ class ViewerCommand(PluginCommand):
                 Console.ok(f"...killing process {p}")
                 os.kill(p, signal.SIGKILL)
 
+        if arguments.stop:
 
-        else:
+            stop()
+
+        elif arguments.start:
+
+            stop()
+
             import cloudmesh.viewer as viewer
+
+
             location = inspect.getfile(viewer)
             for i in range(0,3):
                 location = os.path.dirname(location)
@@ -55,5 +63,23 @@ class ViewerCommand(PluginCommand):
             electron = subprocess.Popen(f"yarn {options}",
                                         cwd=location,
                                         shell=True)
+
+        elif arguments.install:
+            stop()
+
+            import cloudmesh.viewer as viewer
+            location = inspect.getfile(viewer)
+            for i in range(0,3):
+                location = os.path.dirname(location)
+            if arguments.OPTIONS:
+                options = " ".join(arguments.OPTIONS)
+            else:
+                options = ""
+                options = "dev" # till its fixed
+            os.chdir(location)
+            Shell.rmdir(path_expand(f"{location}/node_modules"))
+
+            os.system("pwd")
+            os.system(f"yarn install --force")
 
         return ""
