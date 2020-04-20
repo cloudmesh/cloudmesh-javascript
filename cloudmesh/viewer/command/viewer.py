2aa0f9cd80f1a6fbfe6890c71135f36c2a611108
Gregor von Laszewski <laszewski@gmail.com>
2020-04-20 05:53:18 -0400

    add the deploy template

--- cloudmesh/viewer/command/viewer.py
+++ cloudmesh/viewer/command/viewer.py
@@ -21,16 +21,19 @@ class ViewerCommand(PluginCommand):
         ::
 
           Usage:
-                viewer install
                 viewer stop
                 viewer start [OPTIONS...]
+                viewer deploy --uninstall
+                viewer deploy --branch=BRANCH
+                viewer deploy
 
           This command starts the javascript GUI
 
 
         """
 
-        def stop():
+
+        if arguments.stop:
             found = []
             processes = Shell.ps()
             for p in processes:
@@ -41,45 +44,49 @@ class ViewerCommand(PluginCommand):
                 Console.ok(f"...killing process {p}")
                 os.kill(p, signal.SIGKILL)
 
-        if arguments.stop:
-
-            stop()
 
         elif arguments.start:
-
-            stop()
-
             import cloudmesh.viewer as viewer
-
-
             location = inspect.getfile(viewer)
             for i in range(0,3):
                 location = os.path.dirname(location)
             if arguments.OPTIONS:
                 options = " ".join(arguments.OPTIONS)
             else:
-                options = ""
-                options = "dev" # till its fixed
+                options = "build"
             electron = subprocess.Popen(f"yarn {options}",
                                         cwd=location,
                                         shell=True)
+            if options == "buuld":
+                Console.error("immplement the deploy for now use dev")
 
-        elif arguments.install:
-            stop()
+        elif arguments.deploy and arguments["--uninstall"]:
 
-            import cloudmesh.viewer as viewer
-            location = inspect.getfile(viewer)
-            for i in range(0,3):
-                location = os.path.dirname(location)
-            if arguments.OPTIONS:
-                options = " ".join(arguments.OPTIONS)
-            else:
-                options = ""
-                options = "dev" # till its fixed
-            os.chdir(location)
-            Shell.rmdir(path_expand(f"{location}/node_modules"))
+            Console.error("not yet implemented for Linux")
+            Console.error("not yet implemented for Windows")
+            Console.error("not yet implemented for macOS")
 
-            os.system("pwd")
-            os.system(f"yarn install --force")
+        elif arguments.deploy and arguments["--branch"]:
+
+
+            branch=arguments["--branch"]
+            try:
+                os.system("git checkout {branch}")
+                Console.error("not yet implemented for Linux")
+                Console.error("not yet implemented for Windows")
+                Console.error("not yet implemented for macOS")
+            except Exception as e:
+                print (e)
+
+        elif arguments.deploy:
+            try:
+                os.ssytem("git status")
+                Console.error("not yet implemented for Linux")
+                Console.error("not yet implemented for Windows")
+                Console.error("not yet implemented for macOS")
+            except Exception as e:
+                print (e)
+
+
 
         return ""
