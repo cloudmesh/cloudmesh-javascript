d0cdc46fdc6e0a6f2d08c98233c1ccf5a0f49519
Josh Goodman <jogoodma@iu.edu>
2020-05-09 22:00:29 -0400

    Help docs working in dev, not working in build.

--- renderer/helpers/fileUtils.js
+++ renderer/helpers/fileUtils.js
@@ -1,15 +1,21 @@
 import fs from 'fs'
 import { join } from 'path'
 
-export const getAllFiles = (
-  dirPath,
-  filter = () => true,
-  arrayOfFiles = []
-) => {
+/**
+ * Given a directory, returns a list of all files within
+ * that directory and its tree.
+ *
+ * @param dirPath - The directory path to search.
+ * @param arrayOfFiles - Optional array to add files to.
+ * @returns {*[]} - An array of file paths contained in the directory.
+ */
+export const getAllFiles = (dirPath, arrayOfFiles = []) => {
+  // Read in all directories and files in initial path.
   const files = fs.readdirSync(dirPath)
-  files.filter(filter).forEach((file) => {
+  // Loop over each entry in the directory.
+  files.forEach((file) => {
     if (fs.statSync(join(dirPath, file)).isDirectory()) {
-      arrayOfFiles = getAllFiles(join(dirPath, file), filter, arrayOfFiles)
+      arrayOfFiles = getAllFiles(join(dirPath, file), arrayOfFiles)
     } else {
       arrayOfFiles.push(join(dirPath, file))
     }
--- renderer/pages/help/[...markdown].js
+++ renderer/pages/help/[...markdown].js
@@ -1,42 +1,53 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 import { shell } from 'electron'
 import fs from 'fs'
 import path from 'path'
 import markdownToHtml from '../../helpers/markdownToHtml'
 import { getAllFiles } from '../../helpers/fileUtils'
+import Button from '@material-ui/core/Button'
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos'
 
 const markdownDir = process.cwd()
 
-const HelpDoc = ({ content }) => {
+const HelpDoc = ({ content, showBackButton = true }) => {
+  const router = useRouter()
   return (
     <div
       onClickCapture={(e) => {
         // Capture all external link clicks and redirect it to external browser.
         const href = e?.target?.href
-        if (href && /https?:\/\//.test(href)) {
+        if (href && /https?:\/\/(?!localhost)/.test(href)) {
           // Prevent Electron from trying to open this.
           e.preventDefault()
           // Send link to the external browser.
           shell.openExternal(href)
         }
       }}>
+      {showBackButton && (
+        <Button onClick={() => router.back()}>
+          <ArrowBackIosIcon /> Back
+        </Button>
+      )}
       <div dangerouslySetInnerHTML={{ __html: content }} />
     </div>
   )
 }
 
 export async function getStaticPaths() {
-  const files = getAllFiles(path.join(markdownDir, 'docs'), (file) =>
-    file.endsWith('.md')
-  )
+  // Get all Markdown files.
+  const files = getAllFiles('docs').filter((file) => file.endsWith('.md'))
 
-  console.log(files)
-
-  const paths = [
-    {
-      params: { markdown: ['index.html'] },
-    },
-  ]
+  const paths = files.map((file) => {
+    const markdown = file.split(path.sep)
+    return {
+      params: { markdown },
+    }
+  })
+  // Push the custom mapping of index.html => README.md
+  paths.push({
+    params: { markdown: ['index.html'] },
+  })
 
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
@@ -46,13 +57,21 @@ export async function getStaticPaths() {
 // This also gets called at build time
 export async function getStaticProps({ params }) {
   const filePathArray = params?.markdown ?? []
+  let showBackButton = true
   let content = ''
-  if (filePathArray.length === 1 && filePathArray[0] === 'index.html') {
-    content = await markdownToHtml(
-      fs.readFileSync(path.join(markdownDir, 'README.md'), 'utf8') ?? ''
-    )
+  if (filePathArray.length >= 1) {
+    if (filePathArray[0] === 'index.html') {
+      showBackButton = false
+      content = await markdownToHtml(
+        fs.readFileSync(path.join(markdownDir, 'README.md'), 'utf8') ?? ''
+      )
+    } else {
+      content = await markdownToHtml(
+        fs.readFileSync(path.join(markdownDir, ...filePathArray), 'utf8') ?? ''
+      )
+    }
   }
-  return { props: { content } }
+  return { props: { content, showBackButton } }
 }
 
 export default HelpDoc
